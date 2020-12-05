5d2edccfc5b060427d9641ea6af6e4a2b4abe1ea
jaime <jgarciadiaz@groupon.com>
2020-12-04 18:24:51 -0600

    chore: set node version

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const cron = require('node-cron');
 const postsFromAPI = require('./etl/posts-from-api')
 
 function main() {
+  console.log('executing main')
   cron.schedule('*/5 * * * *', async () => {
     await postsFromAPI();
   });
