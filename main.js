23715a723c8dab9753c5e76047deeb227f0ddd66
jaime <jgarciadiaz@groupon.com>
2020-12-06 21:17:27 -0600

    feat: hit homepage

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 const debug = require('debug')('app:main');
 const cron = require('node-cron');
 const express = require('express')
+const fetch = require('node-fetch');
 
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => res.render(':)'))
+  .get('/', (req, res) => {
+    debug('homepage')
+    return res.render(':)')
+  })
   .listen(PORT, () => debug(`Listening on ${ PORT }`))
 
 const postsFromAPI = require('./etl/posts-from-api')
 
 function main() {
   cron.schedule('*/5 * * * *', async () => {
+    await fetch('https://chicago-food-20.herokuapp.com/');
+
     debug('postsFromAPI')
     await postsFromAPI();
   });
